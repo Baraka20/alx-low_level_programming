f5b9a9997dd43fa42c90c052adc405abd5f31725
Mercy Baraka <mercywekesa17@gmail.com>
2022-09-08 15:38:38 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,27 +1,19 @@
 #!/bin/bash
 #include <stdio.h>
-
-
-
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-int main(void)
-
-{                        
-	chat myChar(55);
-
-	strcpy(myChar ,
-"/"Programming is like building a multlngualpuzzle")
-
-		puts(mychar):
-
-	        return (0);
-)
+ #include <string.h>
+ 
+ /**
+  *   *  * main - Entry point
+  *     *   *
+  *       *    * Return: Always 0 (Success)
+  *         */
+ int main(void)
+	 {
+		      char myChar[55];
+		   
+		       strcpy(myChar, "\"Programming is like building a multilingual puzzle");
+		    
+		        puts(myChar);
+		     
+		         return (0);
+		      }
