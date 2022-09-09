5f9b3111501a1c4c9c329eeb08f6b3f5fee0d43a
Mercy Baraka <mercywekesa17@gmail.com>
2022-09-09 02:35:12 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
- #include <string.h>
- 
- /**
-  *   *  * main - Entry point
-  *     *   *
-  *       *    * Return: Always 0 (Success)
-  *         */
- int main(void)
-	 {
-		      char myChar[55];
-		   
-		       strcpy(myChar, "\"Programming is like building a multilingual puzzle");
-		    
-		        puts(myChar);
-		     
-		         return (0);
-		      }
+/**
+*main - Entry point
+*/int main(void)
+{
+puts("/"Programming is like building a multilingual puzzle");	 
+return (0)
+}
