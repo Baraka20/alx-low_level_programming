931f5048ed2e9f16332dba82c5ed73537c1f3919
Mercy Baraka <mercywekesa17@gmail.com>
2022-09-09 02:43:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
-/**
-*main - Entry point
-*/int main(void)
+
+
+
+/*
+ *
+ * main 
+ *
+ * */int main(void)
+
 {
-puts("/"Programming is like building a multilingual puzzle");	 
-return (0)
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+
 }
