6b848fddb7268f02d89d45970a674b022ef5f93f
Mercy Baraka <mercywekesa17@gmail.com>
2022-09-09 03:42:05 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /*
- *main - Entry pont
- *Return: Always 0 (Success)
- */int main(void)
+*main - Entry pont
+*/int main(void)
 {printf("with proper grammar, but the outcome is a piece of art,\n"
 return (0);
 }
