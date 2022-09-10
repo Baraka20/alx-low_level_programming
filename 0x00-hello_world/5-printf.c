13dab8f564357479ec4a831f720d52911957862c
Mercy Baraka <mercywekesa17@gmail.com>
2022-09-10 06:48:17 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
-#include <stdio.h
+#include <stdio.h>
 
 /*
 *not allowed to use the function puts*/int main(void)
-{printf("with proper grammar, but the outcome is a piece of art,\n"
+{printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
