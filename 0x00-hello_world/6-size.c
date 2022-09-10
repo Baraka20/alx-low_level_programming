06612ca0ab705cd1cffd3eba328de3a4f18d4694
Mercy Baraka <mercywekesa17@gmail.com>
2022-09-10 07:01:09 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 
-/*
+/**
 
  * main -Entry point!
-
+ *
  * Description: prints the size of various types on the computer it is compiled
-
+ *
  * Return: Always 0 (Success)
-
+ *
  */
 int main(void)
 
