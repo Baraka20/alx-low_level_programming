b9f4b0ab5e680023116d0a5264935d90e9071cda
Mercy Baraka <mercywekesa17@gmail.com>
2022-09-12 05:12:53 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*\
+int main(void)
+{
+char alphavet = 'a;
+while (alphabet <= 'z')
+
+{
+if (alphabet == 'q')
+continue;
+if (alphabet == 'e')
+continue;
+
+putchar (alphabe)t;
+alphabet++;
+}
+
+putchar('\n');
+return (0);
+}
