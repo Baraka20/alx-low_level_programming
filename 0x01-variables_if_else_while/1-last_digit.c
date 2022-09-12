203e2cd30a6812acbcf96e82e7feaf5e794716d2
Mercy Baraka <mercywekesa17@gmail.com>
2022-09-12 03:44:18 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ printf("Last digit of %i is %i and is greater than 5\n", n, m);
 }
 else if (m == 0)
 {
-printf("Last digit of %i is %i and is zero\n", n, m);
+printf("Last digit of %i is %i and is 0\n", n, m);
 }
 else if (m < 6 && m != 0)
 {
