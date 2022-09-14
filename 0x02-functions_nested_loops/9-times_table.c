90da0f6af9af276a27d1ac0bfe4576d5fb136a14
Mercy Baraka <mercywekesa17@gmail.com>
2022-09-14 11:40:55 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,16 +3,16 @@
 *print  9 times table
 */void times_table(void)
 {
-int num, mult prod;
+int num, mult, prod;
 for (num = 0; num <= 9; num++)
 {
 _putchar('0');
 for (mult = 1; mult <= 9; mult++)
 {
--putchar(' ');
+_putchar(' ');
 _putchar(',');
 prod = num * mult
-if (prod <= 9)
+if (prod <= 9);
 _putchar(' ');
 else
 _putchar((prod / 10) + '0');
