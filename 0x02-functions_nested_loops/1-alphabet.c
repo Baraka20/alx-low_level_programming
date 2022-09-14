4ed337b2b1176f2b890c6817c1477fc29821a8e8
Mercy Baraka <mercywekesa17@gmail.com>
2022-09-14 01:04:02 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,8 +4,7 @@
 *main - print lowercase alphabet
 *
 *Return: Always 0 (Success)
-*/
-void print_alphabet(void)
+*/void print_alphabet(void)
 {
 char alphabet;
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
