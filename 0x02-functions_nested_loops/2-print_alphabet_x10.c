92e22d9db13788318a44245c6c881f90329df784
Mercy Baraka <mercywekesa17@gmail.com>
2022-09-14 01:22:38 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/*
+* main - alphabet 10x
+*
+*Return: Always 0 (Success)
+*/void print_alphabet_x10(void)
+
+{
+char alphabet;
+int i;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+for (i = 0; i <= 9; i++)
+{
+}
+_putchar(alphabet);
+}
+_putchar('\n')
+}
