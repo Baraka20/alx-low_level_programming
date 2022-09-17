f656c5c2559613c80494b8bae4a0805d756da0f5
Mercy Baraka <mercywekesa17@gmail.com>
2022-09-17 02:40:27 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/*
+*checks for uppercase
+*Return: 1 if c is uppercase
+*Return: 0 if otherwise
+*/int _isupper(int c)
+{
+If((c >= 'A') && (c <= 'Z'))
+Return(1);
+else
+Return(0);
+}
