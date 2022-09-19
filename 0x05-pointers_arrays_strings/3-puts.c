3c792268f43d12eb99bb377b88a304cada4a68df
Mercy Baraka <mercywekesa17@gmail.com>
2022-09-19 12:55:33 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_puts - function
+*@str: parameter of function
+*/
+void _puts(char *str)
+{
+for (; *str != '\0'; str++)
+{
+_puttchar(*str);
+}
+_putchar('\n');
+}
