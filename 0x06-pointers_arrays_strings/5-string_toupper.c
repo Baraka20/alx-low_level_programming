a43311fff1fc9e4cdd3cb2c1ed55f7ef86d1e6cc
Mercy Baraka <mercywekesa17@gmail.com>
2022-09-22 13:15:41 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,18 +4,17 @@
 /**
 * string_toupper - changes all lowercase letters to uppercase
 * @str: string to be changed
-*
-* Return: address tp the string
+* Return: address to the string
 */
 
 char *string_toupper(char *str)
 
 {
-int i = '\0';
-while (str(i) != '\0')
+int i = 0;
+while (str(i)
 {
-if (str[i] >= 'a' && str[i] <= 'z')
-str[i] -= 32;
+if (str[i] >= 97 && str[i] <= 122)
+str[i] = str(i) - 32;
 i++;
 }
 return (str);
