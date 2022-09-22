0198c6bd579ee4d8b31327eb713e66019146b9dd
Mercy Baraka <mercywekesa17@gmail.com>
2022-09-22 13:51:57 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,14 +11,12 @@
 
 char *_strcat(char *dest, char *src)
 {
-int len = 0, i;
-while (dest[len])
-len++
-for (i = 0; src[i] != 0; i++)
-[
-dest_len = src[i];
-len += i
-]
-dest_len = '\0';
+
+int i = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index]; index++)
+dest[dest_len++] = src[index];
 return (dest);
+
 }
