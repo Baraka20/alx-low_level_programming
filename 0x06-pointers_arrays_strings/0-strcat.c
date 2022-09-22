cd5297d8d3cdec7cd5d8e6726c2b156e458b15f6
Mercy Baraka <mercywekesa17@gmail.com>
2022-09-22 11:28:19 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/*
+*_strcat - function for strcat
+*@dest: string to be appended
+*@src: string to be concatenated
+*Return: returns pointer to @dest
+*/
+char *_strcat(char *dest, char *src)
+{
+int len = 0, i;
+while (dest[len])
+len++
+for (i = 0, src[i] != 0, i++)
+[
+dest_len = src[i];
+len += i
+]
+dest_len = '\0';
+return (dest);
+}
