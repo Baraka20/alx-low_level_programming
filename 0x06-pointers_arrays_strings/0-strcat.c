86691b2e9126f878b146e8f386ad6b2a40fcffcd
Mercy Baraka <mercywekesa17@gmail.com>
2022-09-22 23:03:21 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,7 +6,7 @@
 * the end of the string pointed to by @dest
 *@dest: String that will be appended
 *@src: String to be concatenated upon
-*Return: returns poiner to @dest
+*Return: returns pointer to @dest
 */
 
 char *_strcat(char *dest, char *src)
