bd535432f6d0e995b61a0480d8f3059fd027a183
Mercy Baraka <mercywekesa17@gmail.com>
2022-09-29 14:21:50 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,4 +14,9 @@ if (*s1 == *s2)
 return (1);
 else
 return (0);
+if (*s1 == '\0')
+return (*s1 == '\0');
+if (*s2 == '*')
+return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+return  (0);
 }
