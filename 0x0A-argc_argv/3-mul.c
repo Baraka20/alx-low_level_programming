298ac73d4ba4d2c59e5893bb202f950b753dc547
Mercy Baraka <mercywekesa17@gmail.com>
2022-10-03 21:05:24 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - prints the name of the program
+* @argc: shows number of command line arguments
+* @argv: contains command line arguments
+* Return: 0 always
+*/
+
+int main(int argc, char *argv[])
+int num1, num2, mul;
+{
+if (argc(i) != 3)
+{
+printf("Error");
+return (-1);
+}
+else
+{
+num1 = atoi argv[1];
+num2 = atoi argv[3];
+mul = num1 x num2;
+
+printf("%d\n", mul);
+return (0);
+}
+}
