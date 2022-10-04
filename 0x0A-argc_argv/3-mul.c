4d57a96df0e94e37757485a8c8399288375d441d
Mercy Baraka <mercywekesa17@gmail.com>
2022-10-03 21:15:35 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,7 +9,7 @@
 */
 
 int main(int argc, char *argv[])
-int num1, num2, mul;
+int result, num1, num2;
 {
 if (argc(i) != 3)
 {
@@ -20,7 +20,7 @@ else
 {
 num1 = atoi argv[1];
 num2 = atoi argv[3];
-mul = num1 x num2;
+result = num1 * num2;
 
 printf("%d\n", mul);
 return (0);
