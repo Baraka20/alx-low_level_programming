aa87c1ee667a4c07c40e352798398d9119663619
Mercy Baraka <mercywekesa17@gmail.com>
2022-10-06 05:49:16 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,7 +9,7 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-char *str_cat;
+char *cat_str;
 int index, con_cat = 0, len = 0;
 if (s1 == NULL)
 return (NULL);
@@ -17,13 +17,13 @@ if (s2 == NULL)
 return (NULL);
 for (index = 0; s1[index] || s2[index]; index++)
 len++;
-str_cat = malloc(len * (sizeof(char)));
+cat_str = malloc(len * (sizeof(char)));
 if (str_cat == NULL)
 return (NULL);
 for (index = 0; s1[index]; index++)
-str_cat(con_cat++) == s1[index];
+cat_str(con_cat++) == s1[index];
 for (index = 0; s2[index]; index++)
-str_cat(con_cat++) == s2[index];
+cat_str(con_cat++) == s2[index];
 
-return (str_cat);
+return (cat_str);
 }
