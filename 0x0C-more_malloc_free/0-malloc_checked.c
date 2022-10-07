734dcea971298847c907301e613fe38913977916
Mercy Baraka <mercywekesa17@gmail.com>
2022-10-07 05:09:48 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* malloc_checked - function that allocates memory
+* @b: amount of memory to be allocated
+*
+* Return: a pointer to the adrress of first element
+* of b
+*/
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+ptr = malloc(b);
+if (ptr == NULL)
+exit (98);
+return (ptr);
+}
